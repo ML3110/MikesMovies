04e48229e4d13194053226d0f4648e0e44ade719
mattywing <matthew.lewington1@gmail.com>
2020-01-24 17:08:04 +0000

    Added session for login and SQL login

--- login.php
+++ login.php
@@ -1,5 +1,12 @@
 <?php
 
-
-
 ?>
+
+<h1>Login:</h1>
+<form action="checklogin.php" method="POST">
+
+Username: <input type="text" name="username"></br>
+Password: <input type="password" name="password"></br>
+
+<input type="submit" value="Log in">
+</form>
